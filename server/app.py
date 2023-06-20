f9a2b403745fb22776e8caf9f86c68bda5bf8734
Ryan <salvat36@gmail.com>
2023-06-19 22:57:30 -0500

    passing all

--- server/app.py
+++ server/app.py
@@ -4,11 +4,18 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'instance/app.db')}")
 
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, abort
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Activity, Camper
+from models import db, Activity, Camper, Signup
+
+from werkzeug.exceptions import (
+    HTTPException,
+    BadRequest,
+    UnprocessableEntity,
+    InternalServerError,
+)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
@@ -19,9 +26,88 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
+# app.errorhandler(HTTPException)
+# def handle_bad_request(error):
+#     response = jsonify({"error": "404 Camper not found"})
+#     response.status_code = 404
+#     return response
+
+
+
 @app.route('/')
 def home():
-    return ''
+    return '<h1>Home is where the heart is!<h1>'
+
+
+class Campers(Resource):
+
+    def get(self):
+        camper = [camper.to_dict() for camper in Camper.query.all()]
+        return make_response(jsonify(camper), 200)
+    
+    def post(self):
+        data = request.get_json()
+        if not data or "name" not in data or "age" not in data:
+            return {"error": "400: Name and Age are required"}, 400
+        try:
+            camper = Camper(**data)
+            db.session.add(camper)
+            db.session.commit()
+            return make_response(camper.to_dict(), 201)
+        except:
+            return {"error": "400: Validation error"}, 400
+
+
+api.add_resource(Campers, '/campers')
+
+
+class CamperById(Resource):
+    def get(self, id):
+        camper = Camper.query.get(id)
+        if not camper:
+            return {"error": "404: Camper not found"}, 404
+        return make_response(camper.to_dict(), 200)
+api.add_resource(CamperById,'/campers/<int:id>')
+
+
+
+class Activities(Resource):
+    def get(self):
+        activity = [activity.to_dict() for activity in Activity.query.all()]
+        return make_response(jsonify(activity), 202)
+
+api.add_resource(Activities, '/activities')
+
+class ActivityByID(Resource):
+    def delete(self, id):
+        try:
+            activity = db.session.get(Activity, id)
+            db.session.delete(activity)
+            db.session.commit()
+            return make_response("", 204)
+        except:
+            return {"error": "404: Camper not found"}, 404
+api.add_resource(ActivityByID,'/activities/<int:id>')
+
+
+class Signups(Resource):
+    def get(self):
+        signup = [signup.to_dict() for signup in Signup.query.all()]
+        return make_response(jsonify(signup), 200)
+    
+    def post(self):
+        data = request.get_json()
+        try:
+            signup = Signup(**data)
+            db.session.add(signup)
+            db.session.commit()
+            return make_response(signup.to_dict(), 201)
+        except:
+            return {"error": "400: Validation error"}, 400
+api.add_resource(Signups, '/signups')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -22,16 +22,39 @@ class Activity(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    signups = db.relationship('Signup', back_populates='activities')
+
+    serialize_only = ('id', 'name', 'difficulty')
 
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
-
+    
 class Camper(db.Model, SerializerMixin):
     __tablename__ = 'campers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    signups = db.relationship('Signup', back_populates='campers')
+
+    serialize_only = ('id', 'name', 'age')
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("must have a name")
+        return name
+
+    @validates('age')
+    def validate_age(self, key, age):
+        if not 8<= age <=18:
+            raise ValueError("must be between 8-18")
+        return age
+            
 
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
@@ -39,7 +62,21 @@ class Camper(db.Model, SerializerMixin):
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
+    @validates('time')
+    def validate_time(self, key, time):
+        if time not in range(0, 24):
+            raise ValueError
+        return time
+
     id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+    campers = db.relationship('Camper', back_populates='signups')
+    activities = db.relationship('Activity', back_populates='signups')
+
 
     def __repr__(self):
         return f'<Signup {self.id}>'
